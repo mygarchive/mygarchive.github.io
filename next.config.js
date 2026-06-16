da1e35b29a291c8f56e977a59b69080c1798c03f
mygarchive <hossein.hf273@gmail.com>
2026-06-16 16:22:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+  trailingSlash: true,
 };
-
-export default nextConfig;
+module.exports = nextConfig;
